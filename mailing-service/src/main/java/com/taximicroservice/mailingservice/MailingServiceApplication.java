0013e8afd5448508726be804ed624e8dcfb66fac
Jakub Baranski <kubabar1@interia.pl>
2020-08-17 23:59:24 +0200

    Configure netflix eureka naming client in all microservices

--- mailing-service/src/main/java/com/taximicroservice/mailingservice/MailingServiceApplication.java
+++ mailing-service/src/main/java/com/taximicroservice/mailingservice/MailingServiceApplication.java
@@ -2,8 +2,10 @@ package com.taximicroservice.mailingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MailingServiceApplication {
 
     public static void main(String[] args) {
