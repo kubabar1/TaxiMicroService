7a155d395c6a13fdc0d1c016768be3f574959538
Jakub Baranski <kubabar1@interia.pl>
2020-08-19 17:17:14 +0200

    Configure spring config server and connect all microservices to it

--- spring-config-server/src/main/java/com/taximicroservice/springconfigserver/SpringConfigServerApplication.java
+++ spring-config-server/src/main/java/com/taximicroservice/springconfigserver/SpringConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.taximicroservice.springconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class SpringConfigServerApplication {
 
     public static void main(String[] args) {
