73756d7445e0282178c6d133e7f94fac6a6a7db8
Jakub Baranski <kubabar1@interia.pl>
2020-08-20 00:55:45 +0200

    Add entities for user-service

---
+++ user-service/src/main/java/com/taximicroservice/userservice/model/entity/UserEntity.java
@@ -0,0 +1,59 @@
+package com.taximicroservice.userservice.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "pesel")
+    private String pesel;
+
+    @Column(name = "birth_date", columnDefinition = "DATE")
+    private LocalDate birthDate;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "password_salt")
+    private String passwordSalt;
+
+    @Column(name = "creation_date", columnDefinition = "TIMESTAMP")
+    private LocalDateTime creationDate;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private UserSettingsEntity userSettings;
+
+    @ManyToMany(cascade = { CascadeType.ALL })
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = { @JoinColumn(name = "user_id") },
+            inverseJoinColumns = { @JoinColumn(name = "role_id") }
+    )
+    private Set<RoleEntity> roleEntitySet = new HashSet<>();
+
+}
