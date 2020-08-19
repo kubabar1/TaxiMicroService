e2f766bbc642fe1b519687163268b7a13fe53e23
Jakub Baranski <kubabar1@interia.pl>
2020-08-20 01:49:37 +0200

    Modify AppearanceType class

--- user-service/src/main/java/com/taximicroservice/userservice/model/types/AppearanceType.java
+++ user-service/src/main/java/com/taximicroservice/userservice/model/types/AppearanceType.java
@@ -1,4 +1,16 @@
 package com.taximicroservice.userservice.model.types;
 
+import lombok.Data;
+
+@Data
 public enum AppearanceType {
+    LIGHT("light"),
+    DARK("dark");
+
+    String name;
+
+    AppearanceType(String name) {
+        this.name = name;
+    }
+
 }
